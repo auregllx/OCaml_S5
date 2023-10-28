9c3d51ad671997cd96ead12b9778466045badcb4
auregllx <71653986+auregllx@users.noreply.github.com>
2023-10-28 12:35:55 +0200

    Add files via upload
    exercice 3.4

--- functions.ml
+++ functions.ml
@@ -8,7 +8,7 @@ type base = A|C|G|T|WC
 
 type dna = base list
 
-(* 3.1 *)
+(* 3.0 *)
 
 (* Explode : string -> char list *)
 let explode (chaine: string) : char list =
@@ -22,6 +22,8 @@ let explode (chaine: string) : char list =
 ;;
 
 
+(* 3.1 *)
+
 (* base_of_char : char -> base *)
 let base_of_char (caractere : char) : base = 
   let c = Char.uppercase_ascii caractere in 
@@ -32,6 +34,10 @@ let base_of_char (caractere : char) : base =
   |'T' -> T
   | _ -> WC
 ;;
+
+
+
+(* 3.2 *)
 
 (* char_of_base : base -> char *)
 let char_of_base (b : base) : char = 
@@ -56,6 +62,7 @@ let dna_of_string (chaine : string) : dna =
 ;; 
 
 
+(* 3.3 *)
 
 (* strinf_of_dna : dna -> string *)
 let string_of_dna (list : dna) : string = 
@@ -64,13 +71,53 @@ let string_of_dna (list : dna) : string =
 
 
 
+(* 3.4 *)
 
-let () =
-  let tab1 = [A; C; G; T; T; A; C; WC; C; WC] in   
-  (* let str1 = "GTAA..CT" in *)
+(* cut_prefix : 'a list -> 'a list -> 'a list option *)
+let rec cut_prefix pre l = 
+  match (pre, l) with 
+  |([],suf) -> Some suf
+  |(x :: pre', y :: l') when x = y -> cut_prefix pre' l'
+  |_ -> None
+;;
 
+
+(* 3.5 *)
+
+(* first_occ : 'a list -> 'a list -> ('a list * 'a list) option *)
 
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(* let () = *)
+  
+
+  (* TESTS 3.0 à 3.3 *)
+  (*
+  let tab1 = [A; C; G; T; T; A; C; WC; C; WC] in   
+  (* let str1 = "GTAA..CT" in *)  
 
   print_string "string_of_dna :";
   print_newline ();
@@ -78,9 +125,11 @@ let () =
   print_newline ();
   print_string "dna_of_string :";
   print_newline();
+  *)
 
+  (* TESTS 3.4 
+    -> verifié dans le compilateur *)
 
-;;
 
 
 
